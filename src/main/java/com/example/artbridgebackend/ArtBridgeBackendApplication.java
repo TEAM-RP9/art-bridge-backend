397f34d9dfdb0a07400f6a2dbb08dd62af74e096
marinurme <marinurme98@gmail.com>
2026-02-19 23:47:05 +0700

    Initial commit

---
+++ src/main/java/com/example/artbridgebackend/ArtBridgeBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.artbridgebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArtBridgeBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ArtBridgeBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/artbridgebackend/ArtBridgeBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.artbridgebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArtBridgeBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
