64f39c7d62e9ea0a412112b52e249301fb2ee5c2
Ekoorits <enrikoorits@gmail.com>
2026-03-15 19:26:24 +0200

    ARTBR-16: created "User" entity class with JPABuddy and added dependencies to build.gradle
    Added MapStruct for easy Java bean mapping.
    Added OpenAPI for API testing and documenting APIs.
    Added jakarta.validation for @NotNull annotation.

---
+++ src/main/java/com/example/artbridgebackend/persistence/user/User.java
@@ -0,0 +1,44 @@
+package com.example.artbridgebackend.persistence.user;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.Instant;
+
+@Getter
+@Setter
+@Entity
+@NoArgsConstructor
+@Table(name = "\"user\"")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @NotNull
+    @Column(name = "google_id", nullable = false, unique = true)
+    private String googleId;
+
+    @NotNull
+    @Column(name = "email", nullable = false, unique = true)
+    private String email;
+
+    @NotNull
+    @Column(name = "username", nullable = false, length = 50, unique = true)
+    private String username;
+
+    @CreationTimestamp
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private Instant createdAt;
+
+    @UpdateTimestamp
+    @Column(name = "updated_at", nullable = false)
+    private Instant updatedAt;
+
+}
