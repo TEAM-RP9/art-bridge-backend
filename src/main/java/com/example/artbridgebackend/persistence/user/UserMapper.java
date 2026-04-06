44e0f3833dd91c0c8ebf92d4bf014d08ddf8614c
Ekoorits <enrikoorits@gmail.com>
2026-04-06 21:28:29 +0300

    ARTBR-16: Mappings removed
    Source and target used the same name, no mapping annotations needed.

--- src/main/java/com/example/artbridgebackend/persistence/user/UserMapper.java
+++ src/main/java/com/example/artbridgebackend/persistence/user/UserMapper.java
@@ -2,12 +2,9 @@ package com.example.artbridgebackend.persistence.user;
 
 import com.example.artbridgebackend.dto.RegistrationRequestDto;
 import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface UserMapper {
-
-    @Mapping(source = "googleId", target = "googleId")
-    @Mapping(source = "email", target = "email")
+    
     User toUser(RegistrationRequestDto registrationRequestDto);
 }
